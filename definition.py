5219247098b621b571139adc3b76acd9ff577f02
MitsukiUsui <mitsuo.com.mu@gmail.com>
2018-11-11 16:42:03 +0900

    dev: definition.py

---
+++ definition.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from KEGG import scrape_definition
+
+mod_fp = "./data/module.tsv"
+def_fp = "./data/definition.tsv"
+mod_dir = "./data/module"
+
+mod_df = pd.read_csv(mod_fp, sep='\t')
+print("found {} modules".format(len(mod_df)))
+
+dct_lst = []
+for _, row in mod_df.iterrows():
+    fp = "{}/{}.txt".format(mod_dir, row["module_name"])
+    html = open(fp).read()
+    dct = {
+        "module_name": row["module_name"],
+        "definition": scrape_definition(html)
+    }
+    dct_lst.append(dct)
+
+def_df = pd.DataFrame(dct_lst)
+def_df = def_df[["module_name", "definition"]]
+def_df.to_csv(def_fp, sep='\t', index=False)
+print("output {}".format(def_fp))
+
